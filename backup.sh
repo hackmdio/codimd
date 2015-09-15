69538a26ade4317c4ae025d5ced868dde6a2dbcc
Wu Cheng-Han <jacky_cute0808@hotmail.com>
2015-09-15 16:47:03 +0800

    Removed server deploy files, logs and backups directories. Let user choose any daemon tools and paths as they like.

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-path=./backups
-today=$(date +"%Y%m%d")
-timestamp=$(date +"%Y%m%d%H%M%S")
-mkdir -p $path/$today
-pg_dump hackmd > $path/$today/postgresql_$timestamp
-mongodump -d hackmd -o $path/$today/mongodb_$timestamp
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-forever stop hackmd
-DATABASE_URL='change this' \
-MONGOLAB_URI='change this' \
-PORT='80' \
-SSLPORT='443' \
-DOMAIN='change this' \
-forever -a --uid hackmd -l ./logs/hackmd_log.log -o ./logs/hackmd_out.log -e ./logs/hackmd_error.log start app.js
