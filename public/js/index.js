f693399c766248dc0a561f62360efc3bc90d42d1
Cheng-Han, Wu <jackymaxj@gmail.com>
2016-04-20 22:34:52 +0800

    Fix view scroll position might lost when editor changes the last line and might get wrong knob position

--- public/js/index.js
+++ public/js/index.js
@@ -783,12 +783,7 @@ function checkEditorStyle() {
     if (scrollbarStyle == 'overlay' || currentMode == modeType.both) {
         ui.area.codemirrorScroll.css('height', desireHeight + 'px');
         ui.area.codemirrorScroll.css('min-height', '');
-        // workaround simple scroll bar knob
-        // will get wrong position when editor height changed
-        var scrollInfo = editor.getScrollInfo();
-        preventSyncScroll = true;
-        editor.scrollTo(null, scrollInfo.top - 1);
-        editor.scrollTo(null, scrollInfo.top);
+        checkEditorScrollbar();
     } else if (scrollbarStyle == 'native') {
         ui.area.codemirrorScroll.css('height', '');
         ui.area.codemirrorScroll.css('min-height', desireHeight + 'px');
@@ -804,6 +799,15 @@ function checkEditorStyle() {
             lastEditorWidth = ui.area.edit.width();
         }
     });
+}
+
+function checkEditorScrollbar() {
+    // workaround simple scroll bar knob
+    // will get wrong position when editor height changed
+    var scrollInfo = editor.getScrollInfo();
+    preventSyncScroll = true;
+    editor.scrollTo(null, scrollInfo.top - 1);
+    editor.scrollTo(null, scrollInfo.top);
 }
 
 function checkTocStyle() {
@@ -2216,6 +2220,7 @@ editor.on('changes', function (cm, changes) {
         viewportMargin = newViewportMargin;
         windowResize();
     }
+    checkEditorScrollbar();
 });
 editor.on('focus', function (cm) {
     for (var i = 0; i < onlineUsers.length; i++) {
