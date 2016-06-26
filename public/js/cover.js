357ad0b535301f93a4a69690969b07a8bd409423
Cheng-Han, Wu <jackymaxj@gmail.com>
2016-06-26 23:38:28 +0800

    Fix history filter tags and search keyword might not apply after refresh

--- public/js/cover.js
+++ public/js/cover.js
@@ -266,9 +266,23 @@ $(".ui-clear-history").click(function () {
 });
 
 $(".ui-refresh-history").click(function () {
+    var lastTags = $(".ui-use-tags").select2('val'); 
+    $(".ui-use-tags").select2('val', '');
+    historyList.filter();
+    var lastKeyword = $('.search').val();
+    $('.search').val('');
+    historyList.search();
+    
     resetCheckAuth();
     historyList.clear();
-    parseHistory(historyList, parseHistoryCallback);
+    parseHistory(historyList, function (list, notehistory) { 
+        parseHistoryCallback(list, notehistory);
+        $(".ui-use-tags").select2('val', lastTags);
+        $(".ui-use-tags").trigger('change');
+        historyList.search(lastKeyword);
+        $('.search').val(lastKeyword);
+        checkHistoryList();
+    });
 });
 
 $(".ui-logout").click(function () {
