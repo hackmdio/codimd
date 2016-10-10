a5e6b5dd3b661052956f72bbeab88931e7c74315
Wu Cheng-Han <jacky_cute0808@hotmail.com>
2016-10-10 19:48:05 +0800

    Update to support Ukrainian locale

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use(helmet.hsts({
 }));
 
 i18n.configure({
-    locales: ['en', 'zh', 'fr', 'de', 'ja', 'es', 'el', 'pt', 'it', 'tr', 'ru', 'nl', 'hr', 'pl'],
+    locales: ['en', 'zh', 'fr', 'de', 'ja', 'es', 'el', 'pt', 'it', 'tr', 'ru', 'nl', 'hr', 'pl', 'uk'],
     cookie: 'locale',
     directory: __dirname + '/locales'
 });
