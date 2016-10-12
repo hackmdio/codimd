bd6d69d7a70451a9a587c9ee54b6025d1b27f4ce
Wu Cheng-Han <jacky_cute0808@hotmail.com>
2016-10-12 17:47:25 +0800

    Fix to handle checkAllNotesRevision might return null notes

--- app.js
+++ app.js
@@ -512,7 +512,7 @@ process.on('SIGINT', function () {
         if (history.isReady() && realtime.isReady()) {
             models.Revision.checkAllNotesRevision(function (err, notes) {
                 if (err) throw new Error(err);
-                if (notes.length <= 0) {
+                if (!notes || notes.length <= 0) {
                     clearInterval(checkCleanTimer);
                     return process.exit(0);
                 }
