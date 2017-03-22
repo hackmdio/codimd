e751684aa3b144f46ab7e16360a54356c2f555a8
Wu Cheng-Han <jacky_cute0808@hotmail.com>
2017-03-22 15:31:39 +0800

    Update to print info on exit term signals handled

--- app.js
+++ app.js
@@ -648,6 +648,7 @@ process.on('uncaughtException', function (err) {
 
 // install exit handler
 function handleTermSignals () {
+  logger.info('hackmd has been killed by signal, try to exit gracefully...')
   config.maintenance = true
   // disconnect all socket.io clients
   Object.keys(io.sockets.sockets).forEach(function (key) {
