b2c5359d9878aa355ce092d2c2cf3ab9e270a7a6
hiroki yagi <hiro.yagi0245@gmail.com>
2017-04-26 14:33:26 +0900

    Fix port duplication

--- public/js/lib/config/index.js
+++ public/js/lib/config/index.js
@@ -6,7 +6,14 @@ export const domain = window.domain || '' // domain name
 export const urlpath = window.urlpath || '' // sub url path, like: www.example.com/<urlpath>
 export const debug = window.debug || false
 
-export const port = window.location.port
+export const port = (function(domain){
+  var found = domain.match(/:(\d+$)/)
+  if (found && found[1] == window.location.port) { // there are port in domain.
+    return ''
+  } else {
+    return window.location.port
+  }
+}(domain || window.location.hostname))
 export const serverurl = `${window.location.protocol}//${domain || window.location.hostname}${port ? ':' + port : ''}${urlpath ? '/' + urlpath : ''}`
 window.serverurl = serverurl
 export const noteid = urlpath ? window.location.pathname.slice(urlpath.length + 1, window.location.pathname.length).split('/')[1] : window.location.pathname.split('/')[1]
