1c92524c08d99a9233cc38b6055e243cc429ac35
Sheogorath <sheogorath@shivering-isles.com>
2018-06-26 22:25:11 +0200

    Fix broken unicode urls
    It wasn't possible to create unicode based URLs in freeurl mode, because
    the noteid used for the websocket connection is double escaped. When we
    decode it and let socketio-client reencode it, we get the real
    shortid/noteid and can find the note in the database and open the
    connection.
    
    Signed-off-by: Sheogorath <sheogorath@shivering-isles.com>

--- public/js/lib/config/index.js
+++ public/js/lib/config/index.js
@@ -7,7 +7,7 @@ export const debug = window.debug || false
 export const port = window.location.port
 export const serverurl = `${window.location.protocol}//${domain || window.location.hostname}${port ? ':' + port : ''}${urlpath ? '/' + urlpath : ''}`
 window.serverurl = serverurl
-export const noteid = urlpath ? window.location.pathname.slice(urlpath.length + 1, window.location.pathname.length).split('/')[1] : window.location.pathname.split('/')[1]
+export const noteid = decodeURIComponent(urlpath ? window.location.pathname.slice(urlpath.length + 1, window.location.pathname.length).split('/')[1] : window.location.pathname.split('/')[1])
 export const noteurl = `${serverurl}/${noteid}`
 
 export const version = window.version
