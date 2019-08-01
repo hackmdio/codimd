8394ff58e96fd01cfa096821d61dedb89b105a8a
BoHong Li <raccoon@hackmd.io>
2019-08-02 01:03:05 +0800

    refactor: fix lint on public/js/history.js
    Signed-off-by: BoHong Li <raccoon@hackmd.io>

--- public/js/history.js
+++ public/js/history.js
@@ -144,7 +144,7 @@ export function writeHistory (title, tags) {
 }
 
 function writeHistoryToStorage (title, tags) {
-  let data = store.get('notehistory')
+  const data = store.get('notehistory')
   let notehistory
   if (data && typeof data === 'string') {
     notehistory = JSON.parse(data)
@@ -260,7 +260,7 @@ function parseToHistory (list, notehistory, callback) {
     for (let i = 0; i < notehistory.length; i++) {
       // migrate LZString encoded id to base64url encoded id
       try {
-        let id = LZString.decompressFromBase64(notehistory[i].id)
+        const id = LZString.decompressFromBase64(notehistory[i].id)
         if (id && checkNoteIdValid(id)) {
           notehistory[i].id = encodeNoteId(id)
         }
