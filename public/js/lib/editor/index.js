94703dc8b85b521a5d471aaeeb94cc40425c87a2
BoHong Li <raccoon@hackmd.io>
2019-08-04 23:20:44 +0800

    refactor: fix lint on public/js/lib/editor/index.js
    Signed-off-by: BoHong Li <raccoon@hackmd.io>

--- public/js/lib/editor/index.js
+++ public/js/lib/editor/index.js
@@ -76,8 +76,8 @@ export default class Editor {
       },
       'Cmd-Left': 'goLineLeftSmart',
       'Cmd-Right': 'goLineRight',
-      'Home': 'goLineLeftSmart',
-      'End': 'goLineRight',
+      Home: 'goLineLeftSmart',
+      End: 'goLineRight',
       'Ctrl-C': function (cm) {
         if (!isMac && cm.getOption('keyMap').substr(0, 3) === 'vim') {
           document.execCommand('copy')
@@ -513,6 +513,7 @@ export default class Editor {
       this.jumpToAddressBarKeymapValue = null
     }
   }
+
   setOverrideBrowserKeymap () {
     var overrideBrowserKeymap = $(
       '.ui-preferences-override-browser-keymap label > input[type="checkbox"]'
