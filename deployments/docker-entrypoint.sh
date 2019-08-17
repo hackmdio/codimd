1f5b9813cb78e6b65eb3d01e82a79de151529eac
BoHong Li <raccoon@hackmd.io>
2019-08-18 02:37:34 +0800

    feat: add new dockerfile
    Signed-off-by: BoHong Li <raccoon@hackmd.io>

--- deployments/docker-entrypoint.sh
+++ deployments/docker-entrypoint.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
+if [[ "$#" -gt 0 ]]; then
+    exec "$@"
+    exit $?
+fi
+
+# check database and redis is ready
 pcheck -constr "$CMD_DB_URL"
 
-sequelize db:migrate
+# run DB migrate
+NEED_MIGRATE=${CMD_AUTO_MIGRATE:=true}
 
+if [[ "$NEED_MIGRATE" = "true" ]] && [[ -f .sequelizerc ]] ; then
+    npx sequelize db:migrate
+fi
+
+# start application
 node app.js
