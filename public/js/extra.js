d9851f2a28ec72db7574bcb4b37f1bd5e89c5e0a
Yukai Huang <yukaihuangtw@gmail.com>
2019-09-28 18:56:14 +0800

    Support embed OSM
    Signed-off-by: Yukai Huang <yukaihuangtw@gmail.com>

--- public/js/extra.js
+++ public/js/extra.js
@@ -1,5 +1,5 @@
 /* eslint-env browser, jquery */
-/* global moment, serverurl, plantumlServer */
+/* global moment, serverurl, plantumlServer, L */
 
 import Prism from 'prismjs'
 import hljs from 'highlight.js'
@@ -453,6 +453,31 @@ export function finishView (view) {
       console.warn(err)
     }
   })
+  view.find('div.geo.raw').removeClass('raw').each(function (key, value) {
+    const $elem = $(value).parent().parent()
+    const $value = $(value)
+    const content = $value.text()
+    try {
+      if (content.match(/[\d\.\,]+/)) {
+        const [lng, lat, zoom] = content.split(',').map(parseFloat)
+        const position = [lat, lng]
+        $elem.html(`<div class="geo-map"></div>`)
+        const map = L.map($elem.find('.geo-map')[0]).setView(position, zoom || 16)
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '<a href="https://www.openstreetmap.org/">OSM</a>',
+          maxZoom: 18,
+        }).addTo(map)
+        L.marker(position).addTo(map)
+        $elem.addClass('geo')
+      }
+    } catch (err) {
+      $elem.append(`<div class="alert alert-warning">${escapeHTML(err)}</div>`)
+      console.warn(err)
+    }
+  })
+
+
   // image href new window(emoji not included)
   const images = view.find('img.raw[src]').removeClass('raw')
   images.each((key, value) => {
@@ -1013,6 +1038,8 @@ function highlightRender (code, lang) {
     return `<div class="abc raw">${code}</div>`
   } else if (lang === 'vega') {
     return `<div class="vega raw">${code}</div>`
+  } else if (lang === 'geo') {
+    return `<div class="geo raw">${code}</div>`
   }
   const result = {
     value: code
