880e04a8f70ae7ab0649203809e70e3a33626640
BoHong Li <raccoon@hackmd.io>
2020-02-26 10:47:50 +0800

    feat: support multi-row table and row span
    please see https://github.com/RedBug312/markdown-it-multimd-table for more syntax example
    
    Signed-off-by: BoHong Li <raccoon@hackmd.io>

--- public/js/extra.js
+++ public/js/extra.js
@@ -1056,6 +1056,11 @@ md.use(require('markdown-it-mathjax')({
 }))
 md.use(require('markdown-it-imsize'))
 md.use(require('markdown-it-ruby'))
+md.use(require('markdown-it-multimd-table'), {
+  multiline: true,
+  rowspan: true,
+  headerless: false
+})
 
 window.emojify.setConfig({
   blacklist: {
