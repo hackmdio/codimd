209cd29b7117a6e719cb83f70bd5c74dc23979a1
Raccoon <raccoon@hackmd.io>
2020-05-17 19:48:17 +0800

    feat: change base image to node 10.20.1
    Signed-off-by: Raccoon <raccoon@hackmd.io>

--- deployments/build.sh
+++ deployments/build.sh
@@ -2,4 +2,12 @@
 
 CURRENT_DIR=$(dirname "$BASH_SOURCE")
 
-docker build -t hackmdio/codimd -f "$CURRENT_DIR/Dockerfile" "$CURRENT_DIR/.."
+GIT_SHA1="$(git rev-parse HEAD)"
+GIT_SHORT_ID="${SHA1:0:8}"
+GIT_TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
+
+DOCKER_TAG="${GIT_TAG:-$GIT_SHORT_ID}"
+
+docker build -t "hackmdio/codimd:$DOCKER_TAG" -f "$CURRENT_DIR/Dockerfile" "$CURRENT_DIR/.."
+
+docker build --build-arg RUNTIME=node-10-cjk-0baafb79 -t "hackmdio/codimd:$DOCKER_TAG-cjk" -f "$CURRENT_DIR/Dockerfile" "$CURRENT_DIR/.."
